30d9583f24b1c898a2905e3f92fa3789e04e89c2
Dpaola-G <dpaolag17@gmail.com>
2024-09-27 17:02:48 -0500

    Primer commit

---
+++ gas.js
@@ -0,0 +1,9 @@
+class Gas {
+    constructor(presion, volumen, temperatura, masaMolecular) {
+        this.presion = presion;
+        this.volumen = volumen;
+        this.temperatura = temperatura;
+        this.masaMolecular = masaMolecular;
+    }
+}
+calcularMasa()
